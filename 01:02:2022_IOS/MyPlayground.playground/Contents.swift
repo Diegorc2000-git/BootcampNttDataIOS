b6b25e55c8aec6b2d8fe8cbf0b4f8aafef17f0ad
Diego Rodriguez Casillas <diegorc2000@icloud.com>
2022-02-01 13:11:21 +0100

    [WIP] -My first commit on Github -

---
+++ 01:02:2022_IOS/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// -> un comentario en una linea
+/*
+ -> Este es un comentario en varias lineas
+*/
+// MARK: - Tipos Basicos
+
+var nombre: String = "pepe" // var -> variable mutable, se puede cambiar
+let apellido: String = "ramirez" // let -> variable inmutable, no se puede cambiar
+
+
+
+var str = "Hello, playground"
