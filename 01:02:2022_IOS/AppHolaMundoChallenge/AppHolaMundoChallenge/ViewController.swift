afd95cbcd9896e9326e6b37231a12662791f1078
Diego Rodriguez Casillas <diegorc2000@icloud.com>
2022-02-02 09:42:28 +0100

    [WIP] -Challenge AppHolaMundo

--- 01:02:2022_IOS/AppHolaMundoChallenge/AppHolaMundoChallenge/ViewController.swift
+++ 01:02:2022_IOS/AppHolaMundoChallenge/AppHolaMundoChallenge/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	// MARK: - IBOutlet
+	
+	// MARK: - Fisrt Screen
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var subtitleLabel: UILabel!
+	@IBOutlet weak var textModificable: UILabel!
+	@IBOutlet weak var centerButton: UIButton!
+	
+	// MARK: - IBActions
+	@IBAction func changeSubtitleLabel(_ sender: UIButton) {
+		self.textModificable.text = "He cambiado este texto"
+	}
+	
+	// MARK: - Second Screen
+
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		configureView()
 	}
 
+	func configureView() {
+		self.titleLabel.text = "Esta es la primera Pagina"
+		self.titleLabel.textColor = UIColor.blue
+		
+		self.subtitleLabel.text = "Estamos En la pantalla de edición"
+		self.subtitleLabel.textColor = UIColor.brown
+		
+		self.textModificable.text = "Este texto va a cambiar"
+		
+		self.centerButton.backgroundColor = UIColor.black
+		self.centerButton.layer.cornerRadius = 5
+	}
 
 }
 
---
+++ 01:02:2022_IOS/AppHolaMundoChallenge/AppHolaMundoChallenge/ViewController2.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController2.swift
+//  AppHolaMundoChallenge
+//
+//  Created by Diego Rodriguez Casillas on 1/2/22.
+//
+
+import Foundation
