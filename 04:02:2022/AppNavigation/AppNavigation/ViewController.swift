f795b41763e5978a23ee373c59b90ef608fc8bd5
Diego Rodriguez Casillas <diegorc2000@icloud.com>
2022-02-04 11:06:09 +0100

    [WIP] init proyecta

---
+++ 04:02:2022/AppNavigation/AppNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNavigation
+//
+//  Created by Diego Rodriguez Casillas on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
